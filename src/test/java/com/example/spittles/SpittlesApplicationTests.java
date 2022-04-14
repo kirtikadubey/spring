969353d2b0286e0a5cc85749bad048382514a025
kirtikadubey <kirtikadubey05@gmail.com>
2022-04-14 11:14:09 -0400

    Initial commit - spring initializer

---
+++ src/test/java/com/example/spittles/SpittlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spittles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpittlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
